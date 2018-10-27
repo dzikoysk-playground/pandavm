c334d4b3e8af21ec237823c8e7c4a067c5abf330
dzikoysk <dzikoysk@dzikoysk.net>
2018-10-27 13:58:25 +0200

    Add Conan package manager

---
+++ conanfile.py
@@ -0,0 +1,35 @@
+from conans import ConanFile, CMake
+
+
+class PandavmConan(ConanFile):
+    name = "pandavm"
+    version = "0.0.1"
+    license = "Apache License 2.0"
+    url = "https://github.com/Panda-Programming-Language/PandaVM"
+    description = "The Panda Programming Language virtual machine"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="hello")
+        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["pandavm"]
+
